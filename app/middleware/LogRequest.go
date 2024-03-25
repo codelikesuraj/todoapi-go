c6dadf5f67135ddc3e6beef1416d67e072e5364f
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-03-25 21:52:04 +0100

    update: log requests to file

--- app/middleware/LogRequest.go
+++ app/middleware/LogRequest.go
@@ -3,15 +3,24 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
+
+func init() {
+	file, err := os.OpenFile("logs/requests.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.SetOutput(file)
+}
 
 func LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
 		log.Printf(
-			" %-5s %-20s %10s",
+			"%s %s (%s)",
 			r.Method,
 			r.URL.RequestURI(),
 			time.Since(start),
